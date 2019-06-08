1529cdb2300491d17c1d2dc32dd613b40aa1edf8
Nikola Djordjevic <nikola.djordjevic04@gmail.com>
2019-06-08 22:37:46 +0200

    angular namesten

---
+++ projects/agent/src/lib/agent.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'agt-agent',
+  template: `
+    <p>
+      agent works!
+    </p>
+  `,
+  styles: []
+})
+export class AgentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/agent/src/projects.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of agent
+ */
+
+export * from './lib/agent.service';
+export * from './lib/agent.component';
+export * from './lib/agent.module';
